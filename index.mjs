9fc41c37f499c801ac4f33e3fc437d3ecef33a58
Christopher Robbertse <chrisjrobbertse@gmail.com>
2023-02-10 00:48:29 +0000

    prototype v1

---
+++ index.mjs
@@ -0,0 +1,19 @@
+// import parser from 'lambda-multipart-parser'
+
+export const handler = async(event) => {
+    console.log('hello from the my zip :)')
+    // const result = await parser.parse(event)
+
+    // console.log(result)
+
+    // TODO implement
+    const response = {
+        statusCode: 200,
+        body: {
+            message: JSON.stringify('Hello from Lambda!'),
+            event: event,
+            customMessage: 'hello its chris'
+        }
+    };
+    return response;
+};
